12a8a6f8d4e797fef4f89da03aa45ee5a9f72e0e
kaliaj <user@email>
2016-07-14 00:20:59 +0000

    Adding the ability to use make with AngioFE2. Tested this on OpenSUSE (swell) and CentOS (tesla).
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Plugins/AngioFE2@8632 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ build/Mkdir.bash
@@ -0,0 +1,10 @@
+set -v
+
+if [ $# == 0 ]; then
+	echo "Usage: Mkdir.bash platform"
+	exit
+fi
+
+mkdir $1
+cd $1
+mkdir AngioFE2
---
+++ build/angiofe2.mk
@@ -0,0 +1,30 @@
+SRC = $(wildcard $(ANGDIR)AngioFE2/*.cpp)
+OBJ = $(patsubst $(ANGDIR)AngioFE2/%.cpp, %.o, $(SRC))
+DEP = $(patsubst $(ANGDIR)AngioFE2/%.cpp, %.d, $(SRC))
+
+
+SO = libangiofe2_$(PLAT).$(SFX)
+LIB = $(ANGDIR)build/lib/$(SO)
+
+FECORE = $(FEBLIB)/libfecore_$(PLAT).a
+
+FEBIOMECH = $(FEBLIB)/libfebiomech_$(PLAT).a
+
+FEBIOLIBS = $(FEBIOMECH) $(FECORE)
+
+$(LIB): $(OBJ)
+ifeq ($(findstring lnx,$(PLAT)),lnx)
+		$(CC) $(LNKFLG) $(DBGFLG) -shared -Wl,-soname,$(SO) -o $(LIB) $(OBJ) $(FEBIOLIBS)
+else ifeq ($(findstring gcc,$(PLAT)),gcc)
+		$(CC) $(LNKFLG) $(DBGFLG) -shared -Wl,-soname,$(SO) -o $(LIB) $(OBJ) $(FEBIOLIBS)
+else
+		$(CC) -dynamiclib $(FLG) -o $(LIB) $(OBJ) $(FEBIOLIBS)
+endif
+
+%.o: $(ANGDIR)AngioFE2/%.cpp
+	$(CC) $(INC) $(DBGFLG) $(FLG) -MMD -c -o $@ $<
+
+clean:
+	$(RM) *.o *.d $(LIB)
+
+-include $(DEP)
